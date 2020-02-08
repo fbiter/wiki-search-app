5712d6a7552c9a41c0706f29af4d9e63f22ce8d0
fichorka <fichorka@gmail.com>
2020-02-08 15:25:37 +0100

    Edit list: disable empty select
    Disable select when there's no items in the searchType

--- src/components/CategoryRow.tsx
+++ src/components/CategoryRow.tsx
@@ -7,10 +7,20 @@ export default function CategoryRow(props) {
       <a className="item-title" href={item.link}>
         {item.title}
       </a>
-      <span className="item-secondary" onClick={() => handleSubcatClick(item)}>
+      <span
+        className="item-secondary"
+        onClick={() => {
+          if (item.subcats > 0) handleSubcatClick(item)
+        }}
+      >
         {item.subcats}
       </span>
-      <a className="item-secondary" onClick={() => handleArticleClick(item)}>
+      <a
+        className="item-secondary"
+        onClick={() => {
+          if (item.pages > 0) handleArticleClick(item)
+        }}
+      >
         {item.pages}
       </a>
     </li>
