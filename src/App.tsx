1202f3d6a2df3f9e6dd6742b24c04309f48b4e5e
fichorka <fichorka@gmail.com>
2020-02-08 15:57:53 +0100

    Update style and layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,22 @@ import {SearchPage} from './pages'
 
 export default function App() {
   return (
-    <div>
-      <Store>
-        <header>
-          <h1 className="header-title">Wiki Search App</h1>
-        </header>
-        <main>
-          <SearchPage />
-        </main>
-      </Store>
-    </div>
+    <>
+      <div className="topPage">
+        <Store>
+          <header>
+            <h1 className="header-title">Wiki Search App</h1>
+          </header>
+
+          <main>
+            <SearchPage />
+          </main>
+        </Store>
+      </div>
+
+      <footer>
+        <span>filip.biterski@gmail.com</span>
+      </footer>
+    </>
   )
 }
